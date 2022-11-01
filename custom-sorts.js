d833f6b522f049dffba5add30e6f0af17e8c68fb
nednguyen <rpmshockwave@gmail.com>
2022-11-01 17:10:48 -0400

    done

--- custom-sorts.js
+++ custom-sorts.js
@@ -3,19 +3,47 @@ function ageSort(users) {
 }
 
 function oddEvenSort(arr) {
-  return arr.sort(() =>);
+  return arr.sort((a, b) => {
+    return (!(a % 2) - !(b % 2)) || (a - b)                   
+  });
 }
 
 function validAnagrams(s, t) {
-  // Your code here
+  let arr = [...s.split(""), ...t.split("")].sort()
+  for (let i = 0; i < arr.length; i += 2) {
+    if (arr[i] !== arr[i + 1]) return false;
+  }
+  return true;
 }
 
 function reverseBaseSort(arr) {
-  // Your code here
+  return arr.sort((a, b) => {
+    return (b.toString().length - a.toString().length) || (a - b)
+  })
 }
 
 function frequencySort(arr) {
-  // Your code here
+  // arr.sort()
+  // let tracker = {};
+  // for (let el of arr) {
+  //   if (el in tracker) {
+  //     tracker[el]++;
+  //   } else {
+  //     tracker[el] = 1;
+  //   }
+  // }
+  // let newArr = Object.entries(tracker)
+  // console.log(newArr)
+  // newArr.sort((a, b) => {
+  //   return a[1] - b[1] || (Number(b[0]) - Number(a[0]))
+  // })
+  // let res = [];
+  // for (let num of newArr) {
+  //   for (let i = 0; i < num[1]; i++) {
+  //     res.push(Number(num[0]))
+  //   }
+  // }
+  // return res;
 }
 
 module.exports = [
